acdc7f4f47e1e20810b886f5c1e748b4a543e904
Larry Custodio <custodioly@gmail.com>
2017-11-08 18:12:05 -0800

    Changed run build to watch for changes

---
+++ src/js/containers/MovieSearchContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class MovieSearchContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Movie Search Container</h1>
+            </div>
+        );
+    }
+}
