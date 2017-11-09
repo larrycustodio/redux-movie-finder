fbdc224a31e9d4bb331d9a8247e23bbac5fa55cf
Larry Custodio <custodioly@gmail.com>
2017-11-08 18:04:53 -0800

    Organizing reducers and stores

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './app';
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 /* eslint-enable */
 
